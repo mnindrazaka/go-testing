93a5c62fddca3f2108da48ca32cf339c3f438fb9
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-11-03 20:13:11 +0700

    feat: testing using testify

--- main_test.go
+++ main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var (
 	kubusKu            Kubus   = NewKubus(4)
@@ -10,22 +14,25 @@ var (
 )
 
 func TestHitungVolume(t *testing.T) {
-	t.Logf("Volume : %.2f", kubusKu.Volume())
-	if kubusKu.Volume() != volumeSeharusnya {
-		t.Errorf("Salah! harusnya %.2f", volumeSeharusnya)
-	}
+	// t.Logf("Volume : %.2f", kubusKu.Volume())
+	// if kubusKu.Volume() != volumeSeharusnya {
+	// 	t.Errorf("Salah! harusnya %.2f", volumeSeharusnya)
+	// }
+	assert.Equal(t, kubusKu.Volume(), volumeSeharusnya, "perhitungan volume salah")
 }
 
 func TestHitungLuas(t *testing.T) {
-	t.Logf("Luas : %.2f", kubusKu.Luas())
-	if kubusKu.Luas() != luasSeharusnya {
-		t.Errorf("Salah! harusnya %.2f", luasSeharusnya)
-	}
+	// t.Logf("Luas : %.2f", kubusKu.Luas())
+	// if kubusKu.Luas() != luasSeharusnya {
+	// 	t.Errorf("Salah! harusnya %.2f", luasSeharusnya)
+	// }
+	assert.Equal(t, kubusKu.Luas(), luasSeharusnya, "perhitungan luas salah")
 }
 
 func TestHitungKeliling(t *testing.T) {
-	t.Logf("Keliling : %.2f", kubusKu.Keliling())
-	if kubusKu.Keliling() != kelilingSeharusnya {
-		t.Errorf("Salah! harusnya %.2f", kelilingSeharusnya)
-	}
+	// t.Logf("Keliling : %.2f", kubusKu.Keliling())
+	// if kubusKu.Keliling() != kelilingSeharusnya {
+	// 	t.Errorf("Salah! harusnya %.2f", kelilingSeharusnya)
+	// }
+	assert.Equal(t, kubusKu.Keliling(), kelilingSeharusnya, "perhitungan keliling salah")
 }
